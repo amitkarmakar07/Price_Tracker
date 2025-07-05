8cdc8a3dbe636bbd64393733e24722b2ba5de6c7
Amit Karmakar <126745396+amitkarmakar07@users.noreply.github.com>
2025-07-05 12:56:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # config.py
 
 PRODUCT_URL = "https://www.flipkart.com/apple-iphone-16-teal-256-gb/p/itm2b7be11cfadef?pid=MOBH4DQFYKJHESFG"
-TARGET_PRICE = 84500
+TARGET_PRICE = 85000
 WEBHOOK_URL = "http://localhost:5000/webhook"
 HEADERS = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
